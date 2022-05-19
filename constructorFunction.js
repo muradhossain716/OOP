03c9a2375736717a990e799ee59826dacf6f5d51
Murad <muradahamed6262@gmail.com>
2022-05-19 15:50:04 +0600

    prototypal inheritence

--- constructorFunction.js
+++ constructorFunction.js
@@ -3,13 +3,20 @@ const Person= function(firstName,lastName){
  this.firstName=firstName;
  this.lastName=lastName;
 }
-const murad= new Person('Murad',1996);
+const murad= new Person('Murad','Ahamed');
 console.log(murad)
-//when a constructor function is called with new keywords 4 things happend
-/*
-1. a new empty object {} is created in constructor function
-2. this keyword of constructor function is assigned to {} empty object means this={}
+const robin=new Person('Robin','Hossain');
+// //when a constructor function is called with new keywords 4 things happend
+// /*
+// 1. a new empty object {} is created in constructor function
+// 2. this keyword of constructor function is assigned to {} empty object means this={}
 
-3.{} linked with prototype
-4. function automatically return the object{}
- */
+// 3.{} linked with prototype
+// 4. function automatically return the object{}
+//  */
+Person.prototype.fullName =function (){
+    return this.firstName+' '+this.lastName;
+}
+console.log(Person)
+console.log(robin.fullName())
+console.log(murad.fullName())
